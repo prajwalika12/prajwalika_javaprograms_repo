28a45b9557aac3a76fb3351739f777afa7e59bb7
welcome <welcome@welcome-PC>
2022-01-29 02:55:18 +0530

    pushing the code first time

---
+++ ProjectEx/src/Package1/Tanu.java
@@ -0,0 +1,20 @@
+package Package1;
+
+public class Tanu 
+{
+	public void m1()
+	{
+		System.out.println("first method");
+	}
+	public void m2()
+	{
+		System.out.println("second method");
+	}
+	public static void main(String[] args) 
+	{
+		Tanu obj=new Tanu();
+		obj.m1();
+		obj.m2();
+	}
+
+}
