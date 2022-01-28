55f7dc500edf649c74f1d773a0a92e3cf5b37136
welcome <welcome@welcome-PC>
2022-01-29 01:44:33 +0530

    pushing the code first time

---
+++ Project1/src/pkg1/ClassA.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class ClassA 
+{
+	public void me1()
+	{
+		System.out.println("ClassA method");
+	}
+	public void me2()
+	{
+		System.out.println("ClassA method");
+	}
+
+}
